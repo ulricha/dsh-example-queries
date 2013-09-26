7be57ea29c3cf8a50d0d2e08de613e151df1a5c9
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2013-09-26 16:21:22 +0200

    Remaining organisation-complex benchmark queries.
    Former-commit-id: 053bad6208082ff070efe77ecb2dafb80fc3788f

--- shredding-benchmarks/organization/organisation-complex.hs
+++ shredding-benchmarks/organization/organisation-complex.hs
@@ -8,6 +8,10 @@ import           Data.Text hiding (all, singleton, length, null)
 
 import qualified Prelude as P
 import           Database.DSH
+import           Database.DSH.Compiler
+
+import           Database.HDBC
+import           Database.HDBC.PostgreSQL
        
 import           Records
 
@@ -15,8 +19,12 @@ import           Records
 -- 1. not null -> EXISTS
 -- 2. better criterion for equijoin predicates when pushing them down
 --    => don't push predicates with free variables not bound by a local generator.
-q1 :: Q [(Text, Text)]
-q1 = [ pair (e_empQ e1) (e_empQ e2)
+-- 3. Split the existential predicate into to two predicates which only correlate
+--    with one of the generators, respectively.
+
+-- Pairs of employees in same department with common task.
+q1c :: Q [(Text, Text)]
+q1c = [ pair (e_empQ e1) (e_empQ e2)
      | e1 <- employees
      , e2 <- employees
      , e_dptQ e1 == e_dptQ e2
@@ -32,8 +40,12 @@ q1 = [ pair (e_empQ e1) (e_empQ e2)
 -- 1. not null -> EXISTS (semijoin)
 -- 2. null -> NOT EXISTS (antijoin)
 -- 3. two levels of existential nesting, with correlation over both levels.
-q2 :: Q [(Employee, Employee)]
-q2 = 
+-- 4. unequality predicate -> thetajoin
+
+-- Pairs of Employees in the same department where x earns less than y and x can
+-- do task y cannot do.
+q2c :: Q [(Employee, Employee)]
+q2c = 
   [ pair e1 e2
   | e1 <- employees
   , e2 <- employees
@@ -52,8 +64,10 @@ q2 =
 thd :: (QA a, QA b, QA c) => Q (a, b, c) -> Q c
 thd (view -> (_, _, c)) = c
   
-q3 :: Q [(Employee, Employee, [Text])]
-q3 = 
+-- Employees where x can do task y cannot do, and x earns less than y, along
+-- with lists of tasks.
+q3c :: Q [(Employee, Employee, [Text])]
+q3c = 
   let tbl = [ tuple3 e1 e2
                      [ t_tskQ t
                      | t <- tasks, t_empQ t == e_empQ e1
@@ -66,4 +80,64 @@ q3 =
             ]
             
   in [ r | r <- tbl, not $ null $ thd r ]
-                      
+  
+-- Employees e1, e2 where e1 and e2 do different tasks, with tagged union of
+-- tasks.
+q4c :: Q [(Employee, Employee, [(Text, Text)])]
+q4c =
+  [ let as = [ pair (toQ "a") (t_tskQ t)
+             | t <- tasks
+             , e_empQ e1 == t_empQ t ]
+        bs = [ pair (toQ "b") (t_tskQ t)
+             | t <- tasks
+             , e_empQ e2 == t_empQ t ]
+    in tuple3 e1 e2 (as ++ bs)
+  | e1 <- employees
+  , e2 <- employees
+  , e_dptQ e1 == e_dptQ e2
+  , e_empQ e1 /= e_empQ e2
+  ]
+  
+
+-- Employees e1, e2 in same department where e1 and e2 do different tasks, with
+-- tagged symmetric difference of tasks.
+q5c :: Q [(Employee, Employee, [(Text, Text)])]
+q5c =
+  let tbl = [ tuple3
+               e1
+               e2
+               ([ pair (toQ "a") (t_tskQ t1)
+                | t1 <- tasks
+                , e_empQ e1 == t_empQ t1
+                , null [ toQ ()
+                       | t2 <- tasks
+                       , t_tskQ t1 == t_tskQ t2
+                       , e_empQ e2 == t_tskQ t2
+                       ]
+                ]
+                ++
+                [ pair (toQ "b") (t_tskQ t1)
+                | t1 <- tasks
+                , e_empQ e1 == t_empQ t1
+                , null [ toQ ()
+                       | t2 <- tasks
+                       , t_tskQ t1 == t_tskQ t2
+                       , e_empQ e1 == t_tskQ t2
+                       ]
+                ])
+            | e1 <- employees
+            , e2 <- employees
+            , e_dptQ e1 == e_dptQ e2
+            , e_empQ e1 /= e_empQ e2
+            ]
+  in [ t | t@(view -> (_, _, c)) <- tbl, not $ null c ]
+
+-- getConn :: IO X100Info
+-- getConn = P.return $ x100Info "localhost" "48130" Nothing
+
+getPGConn :: IO Connection
+getPGConn = connectPostgreSQL "user = 'au' password = 'foobar' host = 'localhost' dbname = 'organisation16'"
+
+main :: IO ()
+main = getPGConn
+       P.>>= (\conn -> debugVL "q1c" conn q1c)
