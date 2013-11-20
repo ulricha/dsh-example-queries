ba06a007366cd08043a163d89ca077bdfdb78c6d
Alexander Ulrich <alexander.ulrich@uni-tuebingen.de>
2013-11-20 15:41:35 +0100

    TPC-H Q5.
    Former-commit-id: ed20e41ea0ddf515cc8dbb7a0b4d940c3a8f228b

---
+++ tpc-h/q5/Q5.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MonadComprehensions   #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RebindableSyntax      #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE ViewPatterns          #-}
+    
+-- TPC-H Q3
+
+module Main where
+
+import qualified Prelude as P
+import Database.DSH
+import Database.DSH.Compiler
+
+import Database.HDBC.PostgreSQL
+
+import Records
+
+project 
+  :: Q ((Integer, Integer, Integer), [((Integer, Integer, Integer), (Double, Double))])
+  -> Q ((Integer, Integer, Integer), Double)
+project gk = pair (fst gk) revenue
+  where
+    revenue = sum [ ep * (1 - d) | (view -> (ep, d)) <- [ snd x | x <- snd gk ] ]
+    
+byRevDate :: Q ((Integer, Integer, Integer), Double) -> Q (Double, Integer)
+byRevDate (view -> (((view -> (_, _, sp)), r))) = pair (r * (-1)) sp
+
+q5 =
+  sortWith (\(view -> (n, r)) -> r * (-1)) $
+  map (\(view -> (k, g)) -> pair k (sum [ e * (1 - d) | (view -> (_, e, d)) <- g ])) $
+  groupWithKey (\(view -> (n, e, d)) -> n) $
+  [ tuple3 (n_nameQ n) (l_extendedpriceQ l) (l_discountQ l)
+  | c <- customers
+  , o <- orders
+  , l <- lineitems
+  , s <- suppliers
+  , n <- nations
+  , r <- regions
+  , c_custkeyQ c == o_custkeyQ o
+  , l_orderkeyQ l == o_orderkeyQ o
+  , l_suppkeyQ l == s_suppkeyQ s
+  , c_nationkeyQ c == s_nationkeyQ s
+  , s_nationkeyQ s == n_nationkeyQ n
+  , n_regionkeyQ n == r_regionkeyQ r
+  , r_nameQ r == (toQ "ASIA")
+  , o_orderdateQ o >= 42
+  , o_orderdateQ o < 60
+  ]
+
+getConn :: IO Connection
+getConn = connectPostgreSQL "user = 'au' password = 'foobar' host = 'localhost' port = '5433' dbname = 'tpch'"
+
+debugQ :: (Show a, QA a) => Q a -> IO ()
+debugQ q = getConn P.>>= \conn -> debugVL "q5" conn q
+
+main :: IO ()
+main = debugQ q5
