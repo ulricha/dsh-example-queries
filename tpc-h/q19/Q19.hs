2992b08d1c29a4e8ec91a0107f6f19bd784b0d5f
Alexander Ulrich <alex@etc-network.de>
2014-04-14 17:37:14 +0200

    TPC-H Q19.
    Former-commit-id: 97ca8e003820bb5c294eb92a0bde424df96f1d5a

---
+++ tpc-h/q19/Q19.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MonadComprehensions   #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RebindableSyntax      #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE ViewPatterns          #-}
+    
+-- TPC-H Q19
+
+module Main where
+
+import qualified Prelude as P
+import Database.DSH
+import Database.DSH.Compiler
+
+import Database.HDBC.PostgreSQL
+
+import Records
+
+between :: Q Integer -> Q Integer -> Q Integer -> Q Bool
+between x l r = x >= l && x <= r
+
+-- Literal transcription of the TPC-H benchmark query
+smPred :: Text -> Double -> Q Part -> Q LineItem -> Q Bool
+smPred brand1 quantity1 p l = 
+  (p_partkeyQ p == l_partkeyQ l)
+  && (p_brandQ p == toQ brand1)
+  && (p_containerQ p `elem` toQ ["SM CASE", "SM BOX", "SM PACK", "SM PKG"])
+  && (l_quantityQ l >= toQ quantity1)
+  && (l_quantityQ l <= toQ quantity1 + 10)
+  && (between (p_sizeQ p) 1 5)
+  && (l_shipmodeQ l `elem` toQ ["AIR", "AIR REG"])
+  && (l_shipinstructQ l == "DELIVER IN PERSON")
+
+medPred :: Text -> Double -> Q Part -> Q LineItem -> Q Bool
+medPred brand2 quantity2 p l = 
+  p_partkeyQ p == l_partkeyQ l
+  && p_brandQ p == toQ brand2
+  && p_containerQ p `elem` toQ ["MED BAG", "MED BOX", "MED PKG", "MED PACK"]
+  && l_quantityQ l >= toQ quantity2
+  && l_quantityQ l<= toQ quantity2 + 10
+  && between (p_sizeQ p) 1 10
+  && l_shipmodeQ l `elem` toQ ["AIR", "AIR REG"]
+  && l_shipinstructQ l == "DELIVER IN PERSON"
+
+lgPred :: Text -> Double -> Q Part -> Q LineItem -> Q Bool
+lgPred brand3 quantity3 p l = 
+  p_partkeyQ p == l_partkeyQ l
+  && p_brandQ p == toQ brand3
+  && p_containerQ p `elem` toQ ["LG CASE", "LG BOX", "LG PACK", "LG PKG"]
+  && l_quantityQ l >= toQ quantity3
+  && l_quantityQ l<= toQ quantity3 + 10
+  && between (p_sizeQ p) 1 15
+  && l_shipmodeQ l `elem` toQ ["AIR", "AIR REG"]
+  && l_shipinstructQ l == "DELIVER IN PERSON"
+  
+
+q19 :: Double -> Double -> Double -> Text -> Text -> Text -> Q Double
+q19 quantity1 quantity2 quantity3 brand1 brand2 brand3 =
+  sum $
+  [ l_extendedpriceQ l * (1 - l_discountQ l)
+  | l <- lineitems
+  , p <- parts
+  , smPred brand1 quantity1 p l
+    || medPred brand2 quantity2 p l
+    || lgPred brand3 quantity3 p l
+  ]
+
+ 
+getConn :: IO Connection
+getConn = connectPostgreSQL "user = 'au' password = 'foobar' host = 'localhost' port = '5432' dbname = 'tpch'"
+
+debugQ :: (Show a, QA a) => String -> Q a -> IO ()
+debugQ s q = getConn P.>>= \conn -> debugVLOpt s conn q
+
+main :: IO ()
+main = debugQ "q19" (q19 1 10 20 "Brand#12" "Brand#23" "Brand#34")
