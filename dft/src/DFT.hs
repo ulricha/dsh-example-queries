2536dd08a13195e7ba58cddd925ed122cb34c77f
Alexander Ulrich <alex@etc-network.de>
2014-06-02 15:29:03 +0200

    DFT: Specialized Radix-4 implementation.
    Former-commit-id: 8af243367d1bca9e5bb3f1071e047a9847111a4a

--- dft/src/DFT.hs
+++ dft/src/DFT.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE MonadComprehensions   #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE RebindableSyntax      #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE UndecidableInstances  #-}
@@ -13,8 +12,6 @@ module Main where
 
 import Debug.Trace
 
-import qualified Prelude as P
-import Database.DSH
 import Database.DSH.Compiler
 
 import Database.X100Client
@@ -25,45 +22,13 @@ import DFT.Radix
 --------------------------------------------------------------------
 -- Test vectors
 
-vec1 :: Q [Complex]
-vec1 = toQ [ (1.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           ]
-
-vec2 :: Q [Complex]
-vec2 = toQ [ (0.0, 0.0)
-           , (1.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           , (0.0, 0.0)
-           ]
-
-vec3 :: Q [Complex]
-vec3 = toQ [ (1, 10)
-           , (2, 20)
-           , (3, 30)
-           , (4, 40)
-           , (5, 50)
-           , (6, 60)
-           , (7, 70)
-           , (8, 80)
-           ]
-
 getConnX100 :: X100Info
 getConnX100 = x100Info "localhost" "48130" Nothing
 
 main :: IO ()
-main = 
+main = do
    -- debugQX100 "dftfast_16" getConnX100 (dftFast 32 2048 (vecFromTable $ vec "v1_2_16"))
    -- debugQX100 "dftfastrec1_16" getConnX100 (dftFastRec1 32 2048 (vecFromTable $ vec "v1_2_16"))
    -- debugQX100 "dftfastrec_16" getConnX100 (dftFastRec 2 32768 (vecFromTable $ vec "v1_2_16"))
-   debugQX100 "radix2_256" getConnX100 (radix2Fft 128 (vecFromTable $ vec "v1_256"))
+   -- debugQX100 "radix2_256" getConnX100 (radix2Fft 128 (vecFromTable $ vec "v1_256"))
+   debugQX100 "radix4_256" getConnX100 (radix4Fft 64 (vecFromTable $ vec "v1_256"))
--- dft/src/DFT/Radix.hs
+++ dft/src/DFT/Radix.hs
@@ -11,6 +11,7 @@
 
 module DFT.Radix 
     ( radix2Fft
+    , radix4Fft
     ) where
 
 import Debug.Trace
@@ -24,8 +25,9 @@ import DFT.Basic
 import DFT.Buneman
 
 ---------------------------------------------------------------------
--- Special case: Radix-2 FFT, directly transcribed from the Wikipedia
--- (imperative) pseudocode.
+-- Special case: Radix-2 FFT (DIT), directly transcribed from the
+-- Wikipedia (imperative) pseudocode:
+-- http://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm
 
 appendPair :: QA a => Q ([a], [a]) -> Q [a]
 appendPair (view -> (as, bs)) = as ++ bs
@@ -41,3 +43,84 @@ radix2Fft n v | otherwise       = trace (show n) $
     odds     = drop (toQ n) subFfts
     subFfts  = concat $ map (radix2Fft (n `P.div` 2)) $ φ $ ρ 2 v
     combine t e o = pair (e .+ t .* o) (e .- t .* o)
+
+---------------------------------------------------------------------
+-- Special case: Radix-4 FFT (DIT).
+-- Source DSP Book, CRC Press 2000
+
+unzip4 :: (QA a, QA b, QA c, QA d) => Q [(a, b, c, d)] -> Q ([a], [b], [c], [d])
+unzip4 abcds = tuple4 (map (\e -> case view e of (a, _, _, _) -> a) abcds)
+                      (map (\e -> case view e of (_, b, _, _) -> b) abcds)
+                      (map (\e -> case view e of (_, _, c, _) -> c) abcds)
+                      (map (\e -> case view e of (_, _, _, d) -> d) abcds)
+
+zip5 :: (QA a, QA b, QA c, QA d, QA e)
+         => Q [a] 
+         -> Q [b] 
+         -> Q [c] 
+         -> Q [d] 
+         -> Q [e] 
+         -> Q [(a, b, c, d, e)]
+zip5 as bs cs ds es = map flatten (zip as (zip bs (zip cs (zip ds es))))
+  where
+    flatten e = tuple5 (fst e)
+                       (fst (snd e))
+                       (fst (snd (snd e)))
+                       (fst (snd (snd (snd e))))
+                       (snd (snd (snd (snd e))))
+
+appendFour :: QA a => Q ([a], [a], [a], [a]) -> Q [a]
+appendFour (view -> (as1, as2, as3, as4)) = as1 ++ as2 ++ as3 ++ as4
+
+zipWith5 :: (QA a, QA b, QA c, QA d, QA e, QA r) 
+         => (Q a -> Q b -> Q c -> Q d -> Q e -> Q r) 
+         -> Q [a] 
+         -> Q [b] 
+         -> Q [c] 
+         -> Q [d] 
+         -> Q [e] 
+         -> Q [r]
+zipWith5 f as bs cs ds es = map (\e -> case view e of (a, b, c, d, e) -> f a b c d e) 
+                                (zip5 as bs cs ds es)
+
+radix4Fft :: Integer -> Q [Complex] -> Q [Complex]
+radix4Fft n v | 4 P.* n P.<= 32 = trace ("dft4 " P.++ show (n P.* 4)) $ dft (4 P.* n) v
+radix4Fft n v | otherwise       = trace ("fft4 " P.++ show (n P.* 4)) $
+    appendFour $ unzip4 $ zipWith5 combine twiddles ys zs gs hs
+  where
+    idxs     = toQ [0 .. n - 1]
+    twiddles = map (\r -> triple (ω n r) (ω n (2 * r)) (ω n (3 * r))) idxs
+    subFfts  = map (radix4Fft $ n `P.div` 4) $ φ $ ρ 4 v
+    ys       = subFfts !! 0
+    zs       = subFfts !! 1
+    gs       = subFfts !! 2
+    hs       = subFfts !! 3
+    combine t y z g h =
+        let (t1, t2, t3) = view t
+            xr1 = y .+ t1 .* z .+ t2 .* g .+ t3 .* h
+            xr2 = y .- t1 .* z .+ t2 .* g .+ t3 .* h
+            xr3 = y .+ t1 .* z .- t2 .* g .+ t3 .* h
+            xr4 = y .+ t1 .* z .+ t2 .* g .- t3 .* h
+        in tuple4 xr1 xr2 xr3 xr4
+
+-- Try this instead of reshape/transpose for shuffling: Might make a
+-- difference when recursion gets deeper and inputs longer.
+
+shuffle4 :: Q [Complex] -> Q [[Complex]]
+shuffle4 vec = zeros <| ones <| twos <| singleton threes
+
+  where
+    zeros  = [ c | (view -> (c, p)) <- vecPos, p `mod` 4 == 0 ]
+    ones   = [ c | (view -> (c, p)) <- vecPos, p `mod` 4 == 1 ]
+    twos   = [ c | (view -> (c, p)) <- vecPos, p `mod` 4 == 2 ]
+    threes = [ c | (view -> (c, p)) <- vecPos, p `mod` 4 == 3 ]
+
+    vecPos = number0 vec
+
+shuffle4' :: Q [Complex] -> Q [[Complex]]
+shuffle4' vec = map filterPos $ toQ [0..3]
+
+  where
+    filterPos i = [ c | (view -> (c, p)) <- vecPos, p `mod` 4 == i ]
+
+    vecPos = number0 vec
