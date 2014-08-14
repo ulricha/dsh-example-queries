aeda09f802f88a7df977ab4b17b08810507fd35e
Alexander Ulrich <alex@etc-network.de>
2014-08-14 16:07:09 +0200

    Test queries.
    Former-commit-id: 63024d768b325bab3d29e9f70a15393324a0ebfe

---
+++ aquery/sql/bestprofit_reasonable.sql
@@ -0,0 +1,7 @@
+select max(running_diff)
+from (select t_price - min(t_price) over
+               (order by t_timestamp
+                rows unbounded preceding)
+               as running_diff
+      from tradessmall
+      where t_tid = 94 and t_tradedate = 15) as t1;
