c53c407fa6ba33e9ad7096a238988bcb7acd0cb1
Alexander Ulrich <alex@etc-network.de>
2014-10-07 17:30:27 +0200

    TPC-H stuff.
    Former-commit-id: ce9d4ffb8a750adb8f5f45eb70a5494a5a978f15

--- tpc-h/Queries/TPCH/Q8.hs
+++ tpc-h/Queries/TPCH/Q8.hs
@@ -23,42 +23,53 @@ import Database.HDBC.PostgreSQL
 
 import Schema.TPCH
 
+data Interval = Interval { iv_start :: Integer, iv_end :: Integer }
+
+inInterval :: Q Integer -> Interval -> Q Bool
+inInterval d interval = d >= toQ (iv_start interval) && d < toQ (iv_end interval)
+
 between :: Q Integer -> Q Integer -> Q Integer -> Q Bool
 between x l r = x >= l && x <= r
 
-allNations :: Q [(Integer, Double, Text)]
-allNations =
-  [ tup3 (o_orderdateQ o) -- should extract the year
-  	 (l_extendedpriceQ l * (1 - l_discountQ l))
-	 (n_nameQ n2)
-  | p <- parts
-  , s <- suppliers
-  , l <- lineitems
-  , o <- orders
-  , c <- customers
+revenue :: Q LineItem -> Q Double
+revenue l = l_extendedpriceQ l * (1 - l_discountQ l)
+
+-- FIXME should extract the year
+allNations :: Text -> Text -> Interval -> Q [(Integer, Double, Text)]
+allNations region typ interval =
+  [ tup3 (o_orderdateQ o) (revenue l) (n_nameQ n2)
+  | p  <- parts
+  , s  <- suppliers
+  , l  <- lineitems
+  , o  <- orders
+  , c  <- customers
   , n1 <- nations
   , n2 <- nations
-  , r <- regions
+  , r  <- regions
   , p_partkeyQ p == l_partkeyQ l
   , s_suppkeyQ s == l_suppkeyQ l
   , l_orderkeyQ l == o_orderkeyQ o
   , o_custkeyQ o == c_custkeyQ c
   , c_nationkeyQ c == n_nationkeyQ n1
   , n_regionkeyQ n1 == r_regionkeyQ r
-  , r_nameQ r == "AMERICA"
+  , r_nameQ r == toQ region
   , s_nationkeyQ s == n_nationkeyQ n2
-  , between (o_orderdateQ o) 23 42
-  , p_typeQ p == "ECONOMY ANODIZED STEEL"
+  , o_orderdateQ o `inInterval` interval
+  , p_typeQ p == toQ typ
   ] 
 
-q8 = 
+nationVolumne :: Text -> Q [(Integer, Double, Text)] -> Q Double
+nationVolumne nation salesInYear =
+    sum [ if n == "BRAZIL" then v else 0
+	| (view -> (_, v, n)) <- salesInYear
+	]
+
+completeVolume :: Q [(Integer, Double, Text)] -> Q Double
+completeVolume salesInYear =
+    sum [ v | (view -> (_, v, n)) <- salesInYear ]
+
+q8 nation = 
   sortWith fst
-  [ pair k
-         ((sum [ if n == "BRAZIL"
-                 then v
-		 else 0
-	       | (view -> (_, v, n)) <- g
-	       ])
-	  / sum [ v | (view -> (_, v, _)) <- g ])
+  [ tup2 k (nationVolumne nation g / completeVolume g)
   | (view -> (k, g)) <- groupWithKey (\(view -> (n, _, _)) -> n) allNations
   ]
