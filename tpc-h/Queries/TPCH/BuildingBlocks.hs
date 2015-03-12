a2aa7b0e34d63d274e9c88e9bfef2875838990fd
Alexander Ulrich <alex@etc-network.de>
2015-03-12 20:58:11 +0100

    Collect common building blocks for TPC-H queries.
    Former-commit-id: 975d073e1fa0d935e5ea39fa56b52d7dbab7e57a

---
+++ tpc-h/Queries/TPCH/BuildingBlocks.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RebindableSyntax    #-}
+
+module Queries.TPCH.BuildingBlocks
+    ( hasNationality
+    , custOrders
+    , orderItems
+    , revenue
+    , orderRevenue
+    , ordersWithStatus
+    ) where
+
+import qualified Prelude as P
+import Database.DSH
+import Schema.TPCH
+
+orderItems :: Q Order -> Q [LineItem]
+orderItems o = [ l | l <- lineitems, l_orderkeyQ l == o_orderkeyQ o ]
+
+custOrders :: Q Customer -> Q [Order]
+custOrders c = [ o | o <- orders, o_custkeyQ o == c_custkeyQ c ]
+
+hasNationality :: Q Customer -> Text -> Q Bool
+hasNationality c nn =
+    or [ n_nameQ n == toQ nn && n_nationkeyQ n == c_nationkeyQ c
+       | n <- nations
+       ]
+
+revenue :: Q LineItem -> Q Decimal
+revenue l = l_extendedpriceQ l * (1 - l_discountQ l)
+
+orderRevenue :: Q Order -> Q Decimal
+orderRevenue o = sum [ revenue l
+                | l <- lineitems
+                , l_orderkeyQ l == o_orderkeyQ o
+                ]
+
+ordersWithStatus :: Text -> Q Customer -> Q [Order]
+ordersWithStatus status c =
+    [ o | o <- custOrders c, o_orderstatusQ o == toQ status ]
