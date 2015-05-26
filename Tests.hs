e38431e83a7bcf607102a46110cd5911f06a16cc
Alexander Ulrich <alex@etc-network.de>
2015-05-26 16:07:14 +0200

    missing file
    Former-commit-id: 5edb63efd236a08dd82cb45253a7fc7fc5e5163a

---
+++ Tests.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Database.DSH.Compiler
+import Database.DSH.Backend.Sql
+import Database.DSH.Backend
+import Database.HDBC.ODBC
+import Queries.TPCH
+import Queries.TPCH.BuildingBlocks
+import Schema.TPCH
+
+conn :: IO SqlBackend
+conn = sqlBackend <$> connectODBC "DSN=tpch1"
+
+runQ8 :: SqlBackend -> IO ()
+runQ8 c = debugQ "q2" c q8Default
+
+main :: IO ()
+main = do
+    c <- conn
+    debugAll c
