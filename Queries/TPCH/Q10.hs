8dd3e18f01c8f53f90cde5ff1d635f1dc0d872e3
Alexander Ulrich <alex@etc-network.de>
2015-05-29 18:53:56 +0200

    Fixed interval specification in Q10.

--- Queries/TPCH/Q10.hs
+++ Queries/TPCH/Q10.hs
@@ -7,7 +7,6 @@
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE UndecidableInstances  #-}
-{-# LANGUAGE ViewPatterns          #-}
 
 -- TPC-H Q10
 
@@ -16,8 +15,9 @@ module Queries.TPCH.Q10
     , q10Default
     ) where
 
-import qualified Data.Time.Calendar as C
+import qualified Data.Time.Calendar          as C
 import           Database.DSH
+import           Queries.TPCH.BuildingBlocks
 import           Schema.TPCH
 
 q10_join :: Day -> Q [((Integer, Text, Decimal, Text, Text, Text, Text), Decimal)]
@@ -36,11 +36,13 @@ q10_join startDate =
     , n <- nations
     , c_custkeyQ c == o_custkeyQ o
     , l_orderkeyQ l == o_orderkeyQ o
-    , o_orderdateQ o >= toQ startDate
-    , o_orderdateQ o < toQ (C.addDays 90 startDate)
+    , o_orderdateQ o `inInterval` interval
     , l_returnflagQ l == "R"
     , c_nationkeyQ c == n_nationkeyQ n
     ]
+
+  where
+    interval = Interval startDate (C.addGregorianMonthsRollOver 3 startDate)
 
 -- | TPC-H Query Q10 with standard validation parameters
 q10Default :: Q [((Integer, Text, Decimal, Text, Text, Text, Text), Decimal)]
