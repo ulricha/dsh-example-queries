84d95e1c40d2a892ab0ce27a452eecdf59a8a2f4
Alexander Ulrich <alex@etc-network.de>
2015-07-17 15:19:35 +0200

    more reasonable indexes on tpch tables

--- scripts/pg/additional_indexes.sql
+++ scripts/pg/additional_indexes.sql
@@ -1,14 +1,23 @@
-create index on part(p_size);
-create index on part(p_type);
-
 create index on region(r_name);
-
 create index on nation(n_name);
 
+-- various property indexes
 create index on customer(c_mktsegment);
+create index on part(p_size);
+create index on part(p_type);
+create index on orders(o_orderstatus);
 
+-- date indexes
 create index on orders(o_orderdate);
-
 create index on lineitem(l_commitdate);
 create index on lineitem(l_shipdate);
 create index on lineitem(l_receiptdate);
+
+-- foreign key indexes
+create index on customer(c_nationkey);
+create index on orders(o_custkey);
+create index on lineitem(l_orderkey);
+create index on supplier(s_nationkey);
+create index on partsupp(ps_partkey);
+create index on partsupp(ps_suppkey);
+create index on nation(n_regionkey);
