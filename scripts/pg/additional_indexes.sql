b22366318214666b2e3a428212228be35ce31397
Alexander Ulrich <alex@etc-network.de>
2015-07-28 07:04:10 +0200

    more tpc-h indexes

--- scripts/pg/additional_indexes.sql
+++ scripts/pg/additional_indexes.sql
@@ -5,7 +5,10 @@ create index on nation(n_name);
 create index on customer(c_mktsegment);
 create index on part(p_size);
 create index on part(p_type);
+create index on part(p_name);
 create index on orders(o_orderstatus);
+create index on lineitem(l_quantity);
+create index on lineitem(l_discount);
 
 -- date indexes
 create index on orders(o_orderdate);
@@ -17,6 +20,9 @@ create index on lineitem(l_receiptdate);
 create index on customer(c_nationkey);
 create index on orders(o_custkey);
 create index on lineitem(l_orderkey);
+create index on lineitem(l_suppkey);
+create index on lineitem(l_partkey);
+create index on lineitem(l_partkey, l_suppkey);
 create index on supplier(s_nationkey);
 create index on partsupp(ps_partkey);
 create index on partsupp(ps_suppkey);
