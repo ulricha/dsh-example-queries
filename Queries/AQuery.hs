2c1b445142989a14ca82297138b7b0250c33aa8e
Alexander Ulrich <alex@etc-network.de>
2016-07-18 16:16:25 +0200

    Code cleanup.

--- Queries/AQuery.hs
+++ Queries/AQuery.hs
@@ -3,7 +3,6 @@
 module Queries.AQuery
     ( module Queries.AQuery.Trades
     , module Queries.AQuery.Packets
-    , debugAll
     ) where
 
 import qualified Data.Time.Calendar       as C
@@ -13,20 +12,3 @@ import           Database.DSH.Compiler
 
 import           Queries.AQuery.Packets
 import           Queries.AQuery.Trades
-
-debugAll :: Backend c => c -> IO ()
-debugAll conn = do
-    putStrLn "bestProfit"
-    debugQ "bestprofit" conn $ bestProfit 614 (C.fromGregorian 2015 12 3)
-
-    putStrLn "last10"
-    debugQ "last10" conn $ last10 42
-
-    putStrLn "flowStats drop"
-    debugQ "flowstats_drop" conn flowStatsZip
-
-    putStrLn "flowStats self"
-    debugQ "flowstats_self" conn flowStatsSelfJoin
-
-    putStrLn "flowStats win"
-    debugQ "flowstats_win" conn flowStatsWin
